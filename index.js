3a56d12fdc52b763910ef6f99fab1333bdff3a1a
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2024-02-06 16:42:01 +0800

    Integrate database

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 
-const app = express()
+require('dotenv').config()
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
+const Note = require('./models/note')
+
+const app = express()
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -34,6 +20,18 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
@@ -44,31 +42,29 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    // .catch(error => {
+    //   console.log(error)
+    //   // response.status(500).end()
+    //   response.status(400).send({ error: 'malformatted id' })
+    // }
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-  return maxId + 1
-}
-
-app.post('/api/notes/', (request, response) => {
-  // const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-
-  // const note = request.body
-  // note.id = maxId + 1
-
+app.post('/api/notes/', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -77,26 +73,53 @@ app.post('/api/notes/', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  // console.log(note)
-  response.json(note)
+  note
+    .save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id != id)
+app.put('/api/notes/:id', (request, response, next) => {
+  // const body = request.body
+  const { content, important } = request.body
 
-  response.status(204).end()
+  // const note = {
+  //   content: body.note,
+  //   important: body.important,
+  // }
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 
---
+++ models/note.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose
+  .connect(url)
+  .then(result => {
+    console.log('connected to MongoDB Atlas')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB Atlas: ', error.message)
+  })
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: 5,
+  },
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Note', noteSchema)
